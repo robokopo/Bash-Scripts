93db291ca8fac8c9b02de42690c35434ff243db5
RoboKopo <pkopocz@gmail.com>
2018-11-13 14:26:48 +0100

    Update machineModel.sh

--- machineModel.sh
+++ machineModel.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Created by robokopo
+# Extension attribute for Jamf Pro to get correct machine model from Apple based on serial number
+
 machineModel=$(curl -s https://support-sp.apple.com/sp/product?cc=`system_profiler SPHardwareDataType | awk '/Serial/ {print $4}' | cut -c 9-` |
 		sed 's|.*<configCode>\(.*\)</configCode>.*|\1|')
 
